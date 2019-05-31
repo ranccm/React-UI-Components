4afd1d8c177aea96ba8fdd883f467eca9d532e3a
ranccm <ranimnapier@gmail.com>
2019-05-31 10:56:30 -0700

    components done. need to add styling

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,17 @@
 import React from 'react';
+import HeaderContent from './HeaderContent';
 import './Header.css';
+
+const HeaderTitle = () => {
+    return (
+        <div>
+            <div>
+                <h1>Lambda School</h1>
+                <span>@LambdaSchool *26 Jan</span>
+                <HeaderContent />
+            </div>
+        </div>
+    );
+};
+
+export default HeaderTitle;
